577b135a92258482043cb71957f6309b66e0bf37
rojitbajimaya <rojitbajimaya@gmail.com>
2025-11-21 19:34:12 +0545

    Add files via upload

---
+++ week-01/WelcomeMessage.java
@@ -0,0 +1,12 @@
+
+public class WelcomeMessage{
+    public static void main (String[] args){
+        
+        System.out.println("Welcome to Programming!");
+        System.out.println("My name is " + " " + args[0] + " " + args[1]);
+        System.out.println("I am learning Java programming");
+        System.out.println("This is my first program");
+        
+    }
+
+}
