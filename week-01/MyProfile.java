bd8da7ed415fcb4fa42e2030e4bf10d38d160447
rojitbajimaya <rojitbajimaya@gmail.com>
2025-11-21 19:33:43 +0545

    Create MyProfile.java

---
+++ week-01/MyProfile.java
@@ -0,0 +1,19 @@
+
+public class MyProfile {
+    public static void main (String[] args){
+        
+        String firstName = args[0];
+        String middleName = args[1];
+        String lastName = args[2];
+        
+        System.out.println("My name is" + " " + firstName + " " + middleName + " " + lastName);
+        
+        System.out.println("My name is" +" " + args[0] + " " + args[1] + " " + args[2]);
+        
+        System.out.println("Trekking");
+        
+        System.out.println("Programming is fun!");
+        
+        
+    }
+}
